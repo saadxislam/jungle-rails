87b64a30bddd18f612531bf98510f9afe65cdac9
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-11-11 21:32:22 +0000

    total product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @total_product_count = Product.sum("quantity")
+    @total_category_count = Category.select('id').uniq.length
+
   end
 end

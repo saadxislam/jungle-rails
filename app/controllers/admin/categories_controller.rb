9bbedf2d51bc1a6e2ccca6706b85f80b87c7b143
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-11-12 04:17:35 +0000

    new feature to create new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,12 +1,31 @@
 class Admin::CategoriesController < ApplicationController
   #actions are methods on the class
   def index
+    @categories = Category.all
   end
   
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :created_at,
+      :updated_at,
+    )
+  end
 end
